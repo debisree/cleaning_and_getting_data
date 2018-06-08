3321af63a40835ad244644aee974b308cd813ae7
Debisree Ray <37156213+debisree@users.noreply.github.com>
2018-06-08 16:31:48 -0500

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+
+
+
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+
+Data <- rbind(train, test)
+colnames(Data) <- c("subject", "activity", featuresWanted.names)
+
+Data$activity <- factor(Data$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+Data$subject <- as.factor(Data$subject)
+
+Data.melted <- melt(Data, id = c("subject", "activity"))
+Data.mean <- dcast(Data.melted, subject + activity ~ variable, mean)
+
+write.table(Data.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
